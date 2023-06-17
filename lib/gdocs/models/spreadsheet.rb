000da863155a49ffa8dd609cd89c0eb1ca4056e6
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-06-17 01:59:50 -0700

    wip - need to check if working, reduce code

--- lib/gdocs/models/spreadsheet.rb
+++ lib/gdocs/models/spreadsheet.rb
@@ -1,10 +1,13 @@
 require 'net/http'
 require 'json'
 
+require 'gdocs/concerns/requests'
+
 module Gdocs
   module Models
     class Spreadsheet
       GOOGLE_SHEETS = "https://sheets.googleapis.com/v4/spreadsheets"
+      include Concerns::Requests
 
       def initialize(token)
         if token.to_s.empty?
@@ -40,19 +43,24 @@ module Gdocs
         self
       end
 
+      # https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/get
+      def run_get(spreadsheet_id)
+        response_body = spreadsheet_get_request("/#{spreadsheet_id}")
+        @data.merge! response_body
+        self
+      end
+
+      def spreadsheet_get_request(url_path)
+        uri = URI(GOOGLE_SHEETS + url_path)
+
+        res = get_request(uri, auth_token: @token)
+        JSON(res.body)
+      end
+
       def spreadsheet_post_request(url_path, body: {})
         uri = URI(GOOGLE_SHEETS + url_path)
 
-        req = Net::HTTP::Post.new(uri.to_s)
-        req.initialize_http_header 'Content-Type' => 'application/json'
-        req['Authorization'] = "Bearer #{@token}"
-
-        req.body = body.to_json
-
-        res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-          http.request(req)
-        end
-
+        res = post_request(uri, auth_token: @token, body: body.to_json)
         response_body = JSON(res.body)
 
         if response_body["error"]
